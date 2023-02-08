c7888a69385a8e48fdf3a8f9a2ebdcfdbbe76d60
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-02-09 00:19:25 +0100

    [none[ delete docker compose; improve register endpoint

--- src/controller/register.py
+++ src/controller/register.py
@@ -9,9 +9,9 @@ from repository import user as repo
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def login(request: Request, user: dto.User):
+    if repo.is_user_exist(user.email):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                        detail=f'User {user.email} exists')
     repo.add_user(user.email, user.password, 'user')
-    # if repo.is_user_exist(user.login, user.email):
-    #     raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-    #                     detail=f'User {user.email} or {user.login} exists')
 
     return {"sdf": "asd"}
--- src/repository/user.py
+++ src/repository/user.py
@@ -1,12 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from entity import user as entity
+from contextlib import contextmanager
 
 engine = create_engine('postgresql://frog:frog!123@127.0.0.1:5400/frog')
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-from contextlib import contextmanager
 
 @contextmanager
 def get_db():
@@ -20,13 +19,22 @@ def get_db():
     finally:
         db.close()
 
+
 def add_user(email: str, password: str, role: str):
     with get_db() as db:
-        # role_id = db.query(entity.role.Role.id).filter(entity.role.Role.role == role).scalar()
+        role_id = db.query(entity.Role.id).filter(
+            entity.Role.role == role).scalar()
         new_user = entity.User(email=email, password=password, role_id=2)
         db.add(new_user)
         db.commit()
 
+
 def get_user(email: str):
     with get_db() as db:
-        return db.query(User).filter(User.email == email).first()
+        return db.query(entity.User).filter(entity.User.email == email).first()
+
+
+def is_user_exist(email: str) -> bool:
+    with get_db() as db:
+        return get_user(email) is not None
+    raise Exception("todo: error")
