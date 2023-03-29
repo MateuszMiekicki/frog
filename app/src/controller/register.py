69c9f1783ac4ad37d39b11bec9bf565c23da0909
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-03-29 02:31:14 +0200

    fix problem with whitespace

--- app/src/controller/register.py
+++ app/src/controller/register.py
@@ -14,4 +14,4 @@ async def login(request: Request, user: User):
                             detail=f'User {user.email} exists')
     repo.add_user(email=user.email,
                   password=hashing.hash(user.password.get_secret_value()), role='user')
-    return {'detail': 'user created'}
+    return {'detail': 'user created'}
