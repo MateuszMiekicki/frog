628514e8fbb47d5fd2a0c92386dee278f1d20d94
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-04-15 23:06:12 +0200

    clear data endpoint

--- app/src/controller/data.py
+++ app/src/controller/data.py
@@ -1,15 +1,5 @@
-from typing import Optional
-from datetime import datetime, timedelta
-from jose import jwt, JWTError
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException, Depends
-from security import hashing
 from fastapi import APIRouter, status, Request, HTTPException, Depends, WebSocket
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-import repository.device as repository
-from dto.device import Device
 router = APIRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 
 @router.websocket("/ws")
