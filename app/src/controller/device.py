dbdb5d9db4714dbe9d87750109d56eef90d7f5f4
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-07-27 18:40:13 +0200

    added auth for config pull

--- app/src/controller/device.py
+++ app/src/controller/device.py
@@ -118,8 +118,17 @@ zmq_config = ConfigForRequest("tcp://toad:5571", 5000)
 
 
 @router.get('/device/{device_id}/configuration', status_code=status.HTTP_200_OK)
-async def get_device_configuration(request: Request, device_id: int):
-    ret = await send_request("test", '''{ "type": "request", "purpose": "configuration", "payload": "1" }''', request.app.state.zmq_context, zmq_config)
+async def get_device_configuration(request: Request, device_id: int, , token: str = Depends(oauth2_scheme)):
+    decode_token = request.app.state.authenticate.decode_token(token)
+    repo = deviceRepository.Device(request.app.state.postgresql)
+    device = repo.get_device_by_id(device_id)
+    if device is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'The device with the id {device_id} was not found.')
+    if device.user_id != decoded_token.get('sub'):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail=f'The device with the id {device_id} is not owned by you.')
+    ret = await send_request(device.mac_address, '''{ "type": "request", "purpose": "configuration", "payload": "{}" }''', request.app.state.zmq_context, zmq_config)
     raise HTTPException(status_code=ret[0], detail=json.loads(ret[1]))
 
 
@@ -129,10 +138,10 @@ async def get_device_configuration(request: Request, device_id: int):
     raise HTTPException(status_code=ret[0], detail=json.loads(ret[1]))
 
 
-@router.post('/device/{device_id}/configuration', status_code=status.HTTP_200_OK)
-async def set_device_configuration(request: Request, device_id: int):
-    mac_address = deviceRepository.Device(
-        request.app.state.postgresql).get_device_by_id(device_id).mac_address
+# @router.post('/device/{device_id}/configuration', status_code=status.HTTP_200_OK)
+# async def set_device_configuration(request: Request, device_id: int):
+#     mac_address = deviceRepository.Device(
+#         request.app.state.postgresql).get_device_by_id(device_id).mac_address
 
-    ret = await send_request(mac_address, "set_config", request.app.state.zmq_context, zmq_config)
-    raise HTTPException(status_code=ret[0], detail=json.loads(ret[1]))
+#     ret = await send_request(mac_address, "set_config", request.app.state.zmq_context, zmq_config)
+#     raise HTTPException(status_code=ret[0], detail=json.loads(ret[1]))
