1782b00e87dc850b760ec98027d64004baa4d676
Mateusz Miekicki <miekickimateusz@gmail.com>
2023-07-29 03:22:54 +0200

    replace bash to sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 exec python ./app/src/main.py --frog_config ./configuration/private/template/frog.properties --databases_config ./configuration/private/template/databases.properties --mailer_config ./configuration/private/template/mailer.properties &
 exec python ./notifier/src/main.py
